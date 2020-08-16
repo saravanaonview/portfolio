a4e366a7f46e146eeae0d7c468349de1d8a2dc33
saravana <saravanaonview@gmail.com>
2020-08-16 07:18:56 -0400

    upto error gandu

--- jobs/models.py
+++ jobs/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Job(models.Model):
+    """docstring for ."""
+    """ models contains table column names """
+    image= models.ImageField(upload_to='images/')
+    summary= models.CharField(max_length=200)
--- jobs/views.py
+++ jobs/views.py
@@ -1,3 +1,9 @@
+print("homepage")
+
+""" from django.Htt
 from django.shortcuts import render
 
 # Create your views here.
+def Home(request):
+    return render(request,'<h1> homepage ipothaiku</h1>')
+"""
